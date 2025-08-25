cfa24bc50d6c07cca5f7fe3bf5055462d5d696be
towa6868 <tanakatowa852@gmail.com>
2025-08-25 13:45:51 +0000

    [meshiterro]finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,34 @@
 class UsersController < ApplicationController
+before_action :set_user, only: [:edit, :update]
+before_action :ensure_correct_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
-    @user = User.find(params[:id])
-  end
+end
 
-  def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
+def update
+  if @user.update(user_params)
     redirect_to user_path(@user.id)
+  else
+    render :edit
   end
+end
+
+private
+
+def set_user
+  @user = User.find_by(id: params[:id])
+end
+
+def ensure_correct_user
+  if @user.nil? || @user.id != current_user.id
+    redirect_to post_images_path
+  end
+end
 
 
   private
@@ -20,4 +36,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+
+  def is_matching_login_user
+  user = User.find_by(id: params[:id])
+  if user.nil? || user.id != current_user.id
+    redirect_to post_images_path
+  end
+end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:name]
 
   has_many :post_images, dependent: :destroy
   has_many :post_comments, dependent: :destroy
